1d2e9f12dfb56b17127fe76ab249f383b37496af
Ueverton-oliveira <ueveron.souz@gmail.com>
2022-07-09 01:11:47 -0400

    feat: add paginates to gategories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category
   include Mongoid::Document
   include Mongoid::Timestamps
+  include NameSearchable
   field :name, type: String
 
   has_many :product_categories
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   it { is_expected.to validate_presence_of(:name) }
+  it_behaves_like "name searchable concern", :category
 
 
   # it { is_expected.to have_many(:product_categories) }
