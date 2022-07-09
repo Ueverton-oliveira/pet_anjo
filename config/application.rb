7defe6f3555f1d52a15245f1b51dcf3019dc4cef
Ueverton-oliveira <ueveron.souz@gmail.com>
2022-07-08 22:51:32 -0400

    feat: add validations

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,10 @@ module PetAnjo
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
     config.i18n.default_locale = :'pt-BR'
 
+    config.api_only = true
+
+    config.autoload_paths += %W["#{config.root}/app/validators/"]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
