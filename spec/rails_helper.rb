f02611bd919eba6fcedc2b2f0406ca8f4dcb5164
Ueverton-oliveira <ueveron.souz@gmail.com>
2022-07-09 00:09:34 -0400

    feat: add shared example render error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'shared_examples', '**', '*.rb')].each { |f| require f }
+
 
 RSpec.configure do |config|
   config.use_active_record = false
